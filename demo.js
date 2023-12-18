e5ff7c12b09707a49c58539e383675387c39d594
Radhika_0014 <79450354+Radhikaverma1429@users.noreply.github.com>
2023-12-18 14:58:28 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,30 @@
+// let a = [1,3,2,54,7,6,4,34,33,12,32,4]
+
+// for (const iterator of a) {
+//     console.log(a.indexOf(iterator));
+// }
+
+// "use strict"
+// var a=10;
+// console.log(a);
+
+// let y =  5 + 5 + "Hello";
+// console.log(y);
+
+// let text;
+// switch (new Date().getDay()) {
+//   case 6:
+//     text = "Today is Saturday";
+//     break;
+//   case 0:
+//     text = "Today is Sunday";
+//     break;
+//   default:
+//     text = "Looking forward to the Weekend";
+// }
+// console.log(text);
+
+const input = require('readline-sync');
+
+let info = input.question("Question text... ");
+console.log(info);
